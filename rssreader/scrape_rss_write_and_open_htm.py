2f6a22e3f48d8d130f48f883ae782e72a0ec6dcd
7ywrxsxrw7z571g@vbit.jp <7ywrxsxrw7z571g@vbit.jp>
2017-11-11 23:59:21 +0900

    Added sc

---
+++ rssreader/scrape_rss_write_and_open_htm.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+import re
+import sys
+import fileinput
+import subprocess
+from xml.etree import ElementTree 
+from urllib.request import Request, urlopen
+
+out_file = open("crowdworksrss.htm","w")
+req = Request('https://crowdworks.jp/public/jobs/group/software_development.rss', headers={'User-Agent': 'Mozilla/5.0'})
+
+f = urlopen(req)
+bytes_content = f.read()  
+
+scanned_text = bytes_content[:1024].decode('ascii', errors='replace')
+match = re.search(r'charset=["\']?([\w-]+)', scanned_text)
+if match:
+    encoding = match.group(1)
+else:
+    encoding = 'utf-8' 
+
+print('encoding:', encoding, file=sys.stderr)
+text = bytes_content.decode(encoding)  
+tree = ElementTree.fromstring(text)
+out_file.write('<!DOCTYPE html><html lang="ja"><meta charset="UTF-8">')
+
+for item in tree.findall('channel/item'):
+    title = item.find('title').text 
+    url = item.find('link').text 
+    html = '<a href = "' + url + '">' + title + '</a><br>'
+    out_file.write(html)
+out_file.write('</html>')
+out_file.close()
+subprocess.run(["open", "crowdworksrss.htm"], stdout=subprocess.PIPE)
