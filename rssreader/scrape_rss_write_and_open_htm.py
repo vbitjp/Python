b4380d48103706a6f3c6fc9fac4403f6d1117549
7ywrxsxrw7z571g@vbit.jp <7ywrxsxrw7z571g@vbit.jp>
2017-11-17 01:52:09 +0900

    Added if __name__ == __main__: statement

--- rssreader/scrape_rss_write_and_open_htm.py
+++ rssreader/scrape_rss_write_and_open_htm.py
@@ -1,40 +1,43 @@
 # coding: utf-8
 import re
-import sys
 import fileinput
 import subprocess
 from xml.etree import ElementTree 
 from urllib.request import Request, urlopen
 
-out_file = open("crowdworksrss.htm","w")
-cwrssList = [["ハードウェア設計・開発", "https://crowdworks.jp/public/jobs/group/hardware_development.rss"], ["Web開発・システム設計", "https://crowdworks.jp/public/jobs/category/241.rss"], ["アプリケーション開発", "https://crowdworks.jp/public/jobs/category/269.rss"], ["アプリ・スマートフォン開発", "https://crowdworks.jp/public/jobs/group/software_development.rss"]]
-out_file.write('<!DOCTYPE html><html lang="ja"><meta charset="UTF-8">')
-for work in cwrssList:
-	h2tag = '<h2>' + work[0] + '</h2>'
-	out_file.write(h2tag)
-	url = work[1]
-	req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+def main():
+	out_file = open("crowdworksrss.htm","w")
+	cwrssList = [["ハードウェア設計・開発", "https://crowdworks.jp/public/jobs/group/hardware_development.rss"], ["Web開発・システム設計", "https://crowdworks.jp/public/jobs/category/241.rss"], ["アプリケーション開発", "https://crowdworks.jp/public/jobs/category/269.rss"], ["アプリ・スマートフォン開発", "https://crowdworks.jp/public/jobs/group/software_development.rss"]]
+	out_file.write('<!DOCTYPE html><html lang="ja"><meta charset="UTF-8">')
+	for work in cwrssList:
+		h2tag = '<h2>' + work[0] + '</h2>'
+		out_file.write(h2tag)
+		url = work[1]
+		req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
 
-	f = urlopen(req)
-	bytes_content = f.read()  
+		f = urlopen(req)
+		bytes_content = f.read()  
 
-	scanned_text = bytes_content[:1024].decode('ascii', errors='replace')
-	match = re.search(r'charset=["\']?([\w-]+)', scanned_text)
-	if match:
-	    encoding = match.group(1)
-	else:
-	    encoding = 'utf-8' 
+		scanned_text = bytes_content[:1024].decode('ascii', errors='replace')
+		match = re.search(r'charset=["\']?([\w-]+)', scanned_text)
+		if match:
+		    encoding = match.group(1)
+		else:
+		    encoding = 'utf-8' 
 
-	#print('encoding:', encoding, file=sys.stderr)
-	text = bytes_content.decode(encoding)  
-	tree = ElementTree.fromstring(text)
-	for item in tree.findall('channel/item'):
-	    title = item.find('title').text 
-	    url = item.find('link').text 
-	    html = '<a href = "' + url + '" target="_blank">' + title + '</a><br>'
-	    out_file.write(html)
-	out_file.write('<br>')
-out_file.write('</html>')
-out_file.close()
-subprocess.run(["open", "crowdworksrss.htm"], stdout=subprocess.PIPE)
-subprocess.run(["rm", "crowdworksrss.htm"], stdout=subprocess.PIPE)
+		#print('encoding:', encoding, file=sys.stderr)
+		text = bytes_content.decode(encoding)  
+		tree = ElementTree.fromstring(text)
+		for item in tree.findall('channel/item'):
+		    title = item.find('title').text 
+		    url = item.find('link').text 
+		    html = '<a href = "' + url + '" target="_blank">' + title + '</a><br>'
+		    out_file.write(html)
+		out_file.write('<br>')
+	out_file.write('</html>')
+	out_file.close()
+	subprocess.run(["open", "crowdworksrss.htm"], stdout=subprocess.PIPE)
+	subprocess.run(["rm", "crowdworksrss.htm"], stdout=subprocess.PIPE)
+
+if __name__ == '__main__':
+	main()
