4d9abf0558da07b9c23fedac4f943a27a8c7fc3e
7ywrxsxrw7z571g@vbit.jp <7ywrxsxrw7z571g@vbit.jp>
2017-12-02 23:58:18 +0900

    Updated settings.py for security

--- django/salon/salon/settings.py
+++ django/salon/salon/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "secret" # hehehe...
+SECRET_KEY = os.environ['DJANGO_KEY01'] # hehehe...
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
