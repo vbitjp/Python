471ce3722c22b3121f0a4511e7f14e1047a9207f
7ywrxsxrw7z571g@vbit.jp <7ywrxsxrw7z571g@vbit.jp>
2017-12-19 23:58:53 +0900

    Added clear() method

--- SpreadSheet/GSSWrite03.py
+++ SpreadSheet/GSSWrite03.py
@@ -18,6 +18,7 @@ def main():
     worksheet.insert_row([7,8], 4)
     print("the value of Cell A4:{num}".format(num = worksheet.cell(4, 1)))
     worksheet.delete_row(1)
+    worksheet.clear()
 
 if __name__ == '__main__':
     main()
