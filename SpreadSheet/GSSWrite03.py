44e1dc0e85dfa4321650b4f4c6a77c148c304df6
7ywrxsxrw7z571g@vbit.jp <7ywrxsxrw7z571g@vbit.jp>
2017-12-21 00:01:03 +0900

    Added some comments into code

--- SpreadSheet/GSSWrite03.py
+++ SpreadSheet/GSSWrite03.py
@@ -14,11 +14,15 @@ def getWorkSheet(argDocId, argKeyPath):
 
 def main():
     worksheet = getWorkSheet(docId, keyPath)
-    worksheet.add_rows(10)
-    worksheet.insert_row([7,8], 4)
-    print("the value of Cell A4:{num}".format(num = worksheet.cell(4, 1)))
-    worksheet.delete_row(1)
-    worksheet.clear()
+    # worksheet.add_rows(10) # セルを10行追加
+    # worksheet.insert_row([7,8], 4) # 4行目のA列からB列に向かって順に値を入力
+    # print("the value of Cell A4:{num}".format(num = worksheet.cell(4, 1))) # A列4行目のセルの値を返す
+    # worksheet.delete_row(1) # 最終行から昇順に、入力した値の分だけ行を削除
+    # worksheet.update_cell(4, 4, 1234) # セルD4に1234を代入
+    # worksheet.update_acell('C4', 1234) # セルC4に1234を代入
+    worksheet.update_acell('C', 1234) # C列全セルに1234を代入
+    # worksheet.clear() # 全セルの値を削除
+
 
 if __name__ == '__main__':
     main()
