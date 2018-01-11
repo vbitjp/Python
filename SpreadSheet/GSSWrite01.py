655fd34b3437682aafdad0e2034e2320028a5b5c
7ywrxsxrw7z571g@vbit.jp <7ywrxsxrw7z571g@vbit.jp>
2018-01-11 23:59:56 +0900

    Add

--- SpreadSheet/GSSWrite01.py
+++ SpreadSheet/GSSWrite01.py
@@ -3,19 +3,20 @@ import os
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-def GSSWrite(argList):
+def getClientAuth():
     scope = ['https://spreadsheets.google.com/feeds']
     docId = os.environ['GOOGLE_GS_ID01']
     keyPath = os.path.expanduser(os.environ['GS_PUBKEY_PATH01'])
 
     credentials = ServiceAccountCredentials.from_json_keyfile_name(keyPath, scope)
     client = gspread.authorize(credentials)
-    gsheet   = client.open_by_key(docId)
-    worksheet  = gsheet.sheet1
-    worksheet.append_row(argList)
+    return client
 
 def main():
-	GSSWrite([1,2,3,4]) #デフォルトのシートであれば、1001行目のA,B,C,D列に1,2,3,4が追加されるはず
+	cl1 = getClientAuth()
+    worksheet = cl1.add_worksheet(title="worksheetABC", rows="20", cols="20") # ワークシートを作成する
+    cl1.del_worksheet(worksheet) # ワークシートを削除する
+
 
 if __name__ == '__main__':
     main()
