bb47c004605fc18c47b192b395f3eebf6207e9f4
7ywrxsxrw7z571g@vbit.jp <7ywrxsxrw7z571g@vbit.jp>
2018-01-21 23:01:39 +0900

    Added append-column for dataframe

--- Pandas/pandas02.py
+++ Pandas/pandas02.py
@@ -48,3 +48,27 @@ print(df)
 2     30      25      12          10          8
 '''
 
+# 行の追加
+'''
+新しい観測データや取引情報を得たとき、それらを既存のDataFrameに
+追加したい場合がある。DataFrame型の変数dfに対してdf.append("Series型のデータ", ignore_index=True)
+を実行すると、渡したSeries型のデータのインデックスがdfのカラムに
+対応した上で新しい行が追加されたDataFrameが生成される。
+dfのカラムとdfに追加するSeries型のデータのインデックスが一致しない場合、
+dfに新しいカラムが追加され、値が存在しない要素はNaNで埋められる。
+'''
+
+# 17行目のリスト変数indexにpineappleを追加
+index.append("pineapple")
+data3 = [30, 12, 10, 8, 25, 3]
+series3 = pd.Series(data3, index=index)
+
+# ここで、24行目のDataFrame変数dfにseries3を追加し、dfに再代入してみる
+df = df.append(series3, ignore_index=True)
+print(df)
+'''
+   apple  orange  banana  strawberry  kiwifruit  pineapple
+0     10       5       8          12          3        NaN
+1     30      25      12          10          8        NaN
+2     30      12      10           8         25        3.0
+'''
