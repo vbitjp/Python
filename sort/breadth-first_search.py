d227e8ef865a125b3b28c6f30e95a484cf978912
7ywrxsxrw7z571g@vbit.jp <7ywrxsxrw7z571g@vbit.jp>
2018-01-26 23:41:57 +0900

    Changed some arguments

--- sort/breadth-first_search.py
+++ sort/breadth-first_search.py
@@ -1,7 +1,7 @@
 from collections import deque
 
 def number_is_threedigit(name):
-      return len(name) == 3
+      return len(name) == 6
 
 graph = {}
 graph["1"] = ["6", "3", "7"]
@@ -10,8 +10,15 @@ graph["6"] = ["9"]
 graph["7"] = ["100", "10"]
 graph["8"] = []
 graph["9"] = []
+graph["10"] = ["1000", "5000", "6000", "10000"]
 graph["100"] = []
-graph["10"] = []
+graph["1000"] = []
+graph["5000"] = []
+graph["6000"] = ["300000"]
+graph["10000"] = ["99999"]
+graph["99999"] = ["200000"]
+graph["200000"] = []
+graph["300000"] = []
 
 def search(name):
     search_queue = deque()
@@ -23,7 +30,7 @@ def search(name):
         # Only search this number if you haven't already searched them.
         if not number in searched:
             if number_is_threedigit(number):
-                print(number + " is 3-digit number!")
+                print(number + " is 6-digit number that initially found!")
                 return True
             else:
                 search_queue += graph[number]
