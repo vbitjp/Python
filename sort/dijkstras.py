1194341392fc85b3a2f07643bf3a22342f6a0599
7ywrxsxrw7z571g@vbit.jp <7ywrxsxrw7z571g@vbit.jp>
2018-02-01 23:58:04 +0900

    first commit

---
+++ sort/dijkstras.py
@@ -0,0 +1,68 @@
+# スタート地点から各ノードへの重み
+graph = {}
+graph["start"] = {}
+graph["start"]["a"] = 6
+graph["start"]["b"] = 2
+
+# Aからゴール、BからA、Bからゴールへの重み
+
+graph["a"] = {}
+graph["a"]["fin"] = 1
+
+graph["b"] = {}
+graph["b"]["a"] = 3
+graph["b"]["fin"] = 5
+
+graph["fin"] = {}
+
+# 各ノードのコスト
+infinity = float("inf") # 無限大
+costs = {}
+costs["a"] = 6
+costs["b"] = 2
+costs["fin"] = infinity
+
+# 親ハッシュテーブル
+parents = {}
+parents["a"] = "start"
+parents["b"] = "start"
+parents["fin"] = None
+
+processed = []
+
+def find_lowest_cost_node(costs):
+    lowest_cost = float("inf")
+    lowest_cost_node = None
+    # 
+    for node in costs:
+        cost = costs[node]
+        # 
+        if cost < lowest_cost and node not in processed:
+            # 
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node
+
+# 
+node = find_lowest_cost_node(costs)
+# 
+while node is not None:
+    cost = costs[node]
+    # 
+    neighbors = graph[node]
+    for n in neighbors.keys():
+        new_cost = cost + neighbors[n]
+        # 
+        if costs[n] > new_cost:
+            # 
+            costs[n] = new_cost
+            # 
+            parents[n] = node
+    # 
+    processed.append(node)
+    # 
+    node = find_lowest_cost_node(costs)
+
+print("Cost from the start to each node:")
+print(costs)
+
