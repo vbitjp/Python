4fcd334c6026f6e2fd776cc5a3cd943fabc0b4a9
7ywrxsxrw7z571g@vbit.jp <7ywrxsxrw7z571g@vbit.jp>
2018-04-11 15:51:36 +0900

    Performed pylint and changed some code

--- rssreader/Post_tweet_with_image.py
+++ rssreader/Post_tweet_with_image.py
@@ -1,58 +1,60 @@
 #!/usr/bin/env python3
 #coding:utf-8
-
-from requests_oauthlib import OAuth1Session
+'''
+This code is for tweet posting with images to Twitter API.
+'''
 import json
 import os
+from requests_oauthlib import OAuth1Session
 
 # 認証(環境変数からAPIキー・シークレット呼び出し)
-consumer_key = os.environ['tw_consumer_key01']
-consumer_secret = os.environ['tw_consumer_secret01']
-access_token = os.environ['tw_access_token01']
-access_token_secret = os.environ['tw_access_token_secret01']
-twitter = OAuth1Session(consumer_key, consumer_secret, access_token, access_token_secret)
+CONSUMER_KEY = os.environ['tw_consumer_key01']
+CONSUMER_SECRET = os.environ['tw_consumer_secret01']
+ACCESS_TOKEN = os.environ['tw_access_token01']
+ACCESS_TOKEN_SECRET = os.environ['tw_access_token_secret01']
+TWITTER = OAuth1Session(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-url_media = "https://upload.twitter.com/1.1/media/upload.json"
-url_text = "https://api.twitter.com/1.1/statuses/update.json"
-path_list_images = [
-	"/Users/vbit/Desktop/images/1.png",
-	"/Users/vbit/Desktop/images/2.jpg",
-	"/Users/vbit/Desktop/images/3.jpg",
-	"/Users/vbit/Desktop/images/4.jpg"
+URL_MEDIA = "https://upload.twitter.com/1.1/media/upload.json"
+URL_TEXT = "https://api.twitter.com/1.1/statuses/update.json"
+PATH_LIST_IMAGES = [
+    "/Users/vbit/Desktop/images/1.png",
+    "/Users/vbit/Desktop/images/2.jpg",
+    "/Users/vbit/Desktop/images/3.jpg",
+    "/Users/vbit/Desktop/images/4.jpg"
 ]
 
 # OAuth認証 セッションを開始
-twitter = OAuth1Session(consumer_key, consumer_secret, access_token, access_token_secret)
+TWITTER = OAuth1Session(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-media_ids = ""
+MEDIA_IDS = ""
 
 # 画像の枚数分ループ
-for path in path_list_images:
+for path in PATH_LIST_IMAGES:
     files = {"media" : open(path, 'rb')}
-    req_media = twitter.post(url_media, files = files)
+    req_media = TWITTER.post(URL_MEDIA, files=files)
 
     # レスポンスを確認
     if req_media.status_code != 200:
-        print ("画像アップデート失敗: {}".format(req_media.text))
+        print("画像アップデート失敗: {}".format(req_media.text))
         exit()
 
     media_id = json.loads(req_media.text)['media_id']
     media_id_string = json.loads(req_media.text)['media_id_string']
-    print ("Media ID: {} ".format(media_id))
+    print("Media ID: {} ".format(media_id))
     # メディアIDの文字列をカンマ","で結合
-    if media_ids == "":
-        media_ids += media_id_string
+    if MEDIA_IDS == "":
+        MEDIA_IDS += media_id_string
     else:
-        media_ids = media_ids + "," + media_id_string
+        MEDIA_IDS = MEDIA_IDS + "," + media_id_string
 
-print ("media_ids: ", media_ids)
+print("media_ids: ", MEDIA_IDS)
 # Media ID を付加してテキストを投稿
-params = {'status': "画像投稿テスト with Python3", "media_ids": [media_ids]}
-res = twitter.post(url_text, params = params)
+PARAMS = {'status': "画像投稿テスト with Python3", "media_ids": [MEDIA_IDS]}
+RES = TWITTER .post(URL_TEXT, params=PARAMS)
 
 # レスポンスを確認
-if res.status_code == 200:
-	print ("OK")
+if RES.status_code == 200:
+    print("OK")
 else:
-    print ("テキストアップデート失敗: %s", res.text)
+    print("テキストアップデート失敗: %s", RES.text)
     exit()
